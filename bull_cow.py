75dd8be3029a019392e15f671a36ce2601ea3684
pheonixal <pheonix97al@gmail.com>
2022-06-24 23:59:21 +0600

    [UPD] Fully working tkinter game

--- bull_cow.py
+++ bull_cow.py
@@ -6,43 +6,60 @@ welcome_text = '''
 In this game you will try to find the hidden number  
 Hidden number consists from 4 digits which not repeat
        You have 15 attempts to find the number'''
+game_win = "Congratulations you won the game of Bulls and Cows"
+game_over = "Sorry but you lost. Right answer was:"
+secret = ''.join(random.sample("0123456789", 4))
+current_guess_count = 1
+tries = 16
+game_frame_crated = False
 
 
 def create_root():
+    """Create main root"""
     root = tk.Tk()
     root.title("Bulls & Cows")
     root.geometry("720x480")
     return root
 
 
-def crate_frame(state):
-    if state == "begin":
-        label_text = welcome_text
-    else:
-        label_text = "Try number "
-
-    welcome_frame = tk.Frame()
-    create_welcome_label(welcome_frame, label_text)
-    welcome_frame.pack()
-    return welcome_frame
+def crate_frame():
+    """Create frame widget"""
+    tk_frame = tk.Frame()
+    tk_frame.pack()
+    return tk_frame
 
 
-def create_welcome_label(welcome_frame, label_text):
-    welcome_label = tk.Label(master=welcome_frame, text=label_text)
-    welcome_label.pack()
+def create_label(given_frame, label_text):
+    """Create label widget for given frame"""
+    frame_label = tk.Label(master=given_frame, text=label_text)
+    frame_label.pack()
 
 
-def create_entry(welcome_frame):
-    entry = tk.Entry(master=welcome_frame)
+def create_entry():
+    """Crete entry widget"""
+    global entry
+    guess = tk.StringVar()
+    entry = tk.Entry(master=game_frame, textvariable=guess)
     entry.pack()
+    entry.bind("<Return>", lambda x: check_bull_cow(guess.get()))
+    entry.focus()
 
 
-def tktk():
+def start_game(welcome_frame):
+    """Removes welcome frame after starting the game"""
+    welcome_frame.pack_forget()
+
+
+def bull_cow_game():
+    """Main function that starts with init"""
+    global secret, current_guess_count, tries
     root = create_root()
-    welcome_frame = crate_frame("begin")
+    # TODO remove welcome frame and leave only one frame
+    welcome_frame = crate_frame()
+    create_label(welcome_frame, welcome_text)
 
-    def handle_click(event):
-        game()
+    def handle_click(self):
+        game(welcome_frame)
 
     button = tk.Button(
         master=welcome_frame,
@@ -58,40 +75,60 @@ def tktk():
     root.mainloop()
 
 
-def check_bull_cow(secret, guess):
-    bull = 0
-    cow = 0
-    for i in range(len(secret)):
-        if secret[i] in guess:
-            cow += 1
-        if secret[i] == guess[i]:
-            bull += 1
-    print(f"{bull}A{cow-bull}B")
-    # return f"{bull}A{cow-bull}B"
+def is_valid_guess(guess):
+    """Check if user given guess is valid"""
+    if len(str(guess)) == 4 and guess.isnumeric():
+        return True
+    return False
 
 
-def game():
-    guess = ''.join(random.sample("0123456789", 4))
-    tries = 1
+def clear_frame():
+    """Clears out all the widgets in given frame"""
+    for widgets in game_frame.winfo_children():
+        print(widgets)
+        widgets.destroy()
 
-    game_frame = crate_frame("game")
-    create_entry(game_frame)
-    #
-    # while tries != 16:
-    #     print("Try number ", tries)
-    #     tries += 1
-    #     try_find = ''
-    #     while len(str(try_find)) != 4:
-    #         try:
-    #             try_find = input()
-    #         except ValueError:
-    #             print("Not an integer!")
-    #     check_bull_cow(guess, try_find)
-    #     if try_find == str(guess):
-    #         print("Congratulations you won the game of Bulls and Cows")
-    #         break
-    print("Hidden number was: ", guess)
+
+def check_bull_cow(guess):
+    """Function to check how many Bulls and Cows user gets after answering"""
+    global secret, current_guess_count
+    if is_valid_guess(guess):
+        if secret == guess:
+            clear_frame()
+            create_label(game_frame, game_win)
+        else:
+            bull = 0
+            cow = 0
+            for i in range(len(secret)):
+                if secret[i] in guess:
+                    cow += 1
+                if secret[i] == guess[i]:
+                    bull += 1
+            current_guess_count += 1
+            entry.pack_forget()
+            create_label(game_frame, f"{bull}A{cow - bull}B")
+            game(False)
+
+
+def game(welcome_frame):
+    global secret, current_guess_count, tries, game_frame, game_frame_crated
+    if welcome_frame:
+        if current_guess_count == 0:
+            start_game(welcome_frame)
+    if current_guess_count == tries:
+        clear_frame()
+        create_label(game_frame, f"{game_over} {secret}")
+    else:
+        if not game_frame_crated:
+            game_frame = crate_frame()
+            game_frame_crated = True
+        label_text = f"Try number {current_guess_count}"
+        create_label(game_frame, label_text)
+        create_entry()
+
+    # Cheat to know wat is hidden number, uncomment to cheat
+    # print("Hidden number was: ", secret)
 
 
 if __name__ == "__main__":
-    tktk()
+    bull_cow_game()
